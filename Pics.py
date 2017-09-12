71fd3ae30377149cd51061cfe609342902d688b6
Yuriy Grabar <yugrabar@cisco.com>
2017-09-12 00:04:41 -0500

    added more

---
+++ Pics.py
@@ -0,0 +1,27 @@
+class Home(object):
+  twitter = 'twitter.png'
+
+
+class Tabs(object):
+  transfers = 'transfers_tab.png'
+  transfers_selected = 'transfers_tab_selected.png'
+
+  class TransferMarket(object):
+    transfers_market = 'transfer_market.png'
+    transfers_market_selected = 'transfer_market_selected.png'
+
+    consumables = 'consumables_tab.png'
+    consumables_selected = 'consumables_tab_selected.png'
+
+    reset_button = 'reset_button.png'
+    search_button = 'search_button.png'
+
+    class Consumables(object):
+      type_player_training = 'type_player_training.png'
+      type_contracts = 'type_contracts.png'
+
+  class TransferTargets(object):
+    transfers_target = 'transfer_target.png'
+    transfers_target_selected = 'transfer_target_selected.png'
+
+
